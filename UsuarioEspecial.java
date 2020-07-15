1aee8c3d4b2c298dbcad1909417c4d8d3e8c0e5d
Walliisons <55769554+wsimao@users.noreply.github.com>
2020-07-15 14:46:36 -0300

    Delete UsuarioEspecial.java

--- UsuarioEspecial.java
+++
@@ -1,7 +0,0 @@
-public class UsuarioEspecial extends Usuario {
-
-
-    public UsuarioEspecial(String nome, String telefone, String email, String cpf,int emprestimoLivro) {
-        super(nome, telefone, email, cpf,emprestimoLivro);
-    }
-}
